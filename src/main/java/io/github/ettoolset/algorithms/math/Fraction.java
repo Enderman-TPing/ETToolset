7e2ac807a87cd2439cda70e6ff881276e2d3e961
Yuheng Wang <enderman142857@outlook.com>
2024-01-31 16:43:12 +0800

    add Fraction.class and delete SimpleTCP

---
+++ src/main/java/io/github/ettoolset/algorithms/math/Fraction.java
@@ -0,0 +1,40 @@
+package io.github.ettoolset.algorithms.math;
+
+public class Fraction {
+    private int numerator;
+    private int dominator;
+
+    public Fraction(){}
+    public Fraction(int dominator,int numerator){
+        this.dominator=dominator;
+        this.numerator=numerator;
+    }
+    public int getNumerator(){
+        return numerator;
+    }
+    public int getDominator(){
+        return dominator;
+    }
+
+    public void setNumerator(int numerator) {
+        this.numerator = numerator;
+    }
+    public void setDominator(int dominator){
+        this.dominator=dominator;
+    }
+    @Override
+    public boolean equals(Object obj){
+        if(!(obj instanceof Fraction)){
+            return false;
+        }else if(((Fraction) obj).getDominator()==this.dominator&&((Fraction) obj).getNumerator()==this.numerator){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return this.numerator+"/"+this.dominator;
+    }
+}
